24208ca93b5ad52fdf4f7d68406b079b2233a6f2
shwan2016 <shwan.it2016@gmail.com>
2016-12-02 20:45:07 -0600

    Add project files.

---
+++ AllEvents/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AllEvents.MvcApplication" Language="C#" %>
---
+++ AllEvents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AllEvents.Startup))]
+namespace AllEvents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
