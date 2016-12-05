02d4411cc55d64d7b8d6d2d59e1fc9b2e6839579
shwan2016 <shwan.it2016@gmail.com>
2016-12-04 19:00:38 -0600

    Adding Users' attending Events Page

---
+++ AllEvents/Controllers/FolloweesController.cs
@@ -0,0 +1,29 @@
+﻿using AllEvents.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AllEvents.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var creators = _context.Followings
+                .Where((f => f.FollowerId == userId))
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(creators);
+        }
+        
+    }
+}
